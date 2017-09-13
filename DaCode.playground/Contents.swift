cf37aafe58a3d49ab130da300b905f7ef402bf8f
Jordan Lejcar <jordan_lejcar@yahoo.com>
2017-09-13 09:21:10 -0500

    We can add and subtract

--- DaCode.playground/Contents.swift
+++ DaCode.playground/Contents.swift
@@ -1,6 +1,1 @@
-var cooltext = ""
-cooltext = "Answer is...\(Double("5")! + Double("78")!)"
-
-var age = 30
-"My age is \(age)"
-
+`
--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -9,42 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var Label: UILabel!
     
-   
+    
     
     @IBOutlet weak var text1: UITextField!
     
-   
+    
     @IBOutlet weak var text2: UITextField!
     
-   
+    
     
     @IBAction func buttontapped(_ sender: Any) {
-        Label.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
         
+        let addition = false
         
-        
-        
-        
-        
+        if addition {
+            Label.text = "Answer: \(Double(text1.text!)! + Double (text2.text!)!)"
+        } else {Label.text = "Answer: \(Double(text1.text!)! - Double(text2.text!)!)"
         }
-       
-
+        
+        
+        
+        
+        
+        
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         Label.text = "The Answer Is!"
     }
-
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
